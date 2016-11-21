922a7ea6926f772f13e2334f7901067d7c9f0bff
oamike <mikewhitmarsh@gmail.com>
2016-11-21 15:06:39 -0800

    updated content types spec to be more explicit, and not rely on ZipFile

--- spec/lib/content_types_spec.rb
+++ spec/lib/content_types_spec.rb
@@ -5,19 +5,43 @@ describe ContentTypes do
   let(:required_content_types) { ['application/vnd.openxmlformats-package.relationships+xml', 'application/vnd.ms-package.3dmanufacturing-3dmodel+xml'] }
   let(:optional_content_types) { ['application/vnd.ms-printing.printticket+xml'] }
 
-	describe ".parse a good file" do
+  let(:zip_entry) { double('ZipEntry') }
 
-    let(:zip_path) { "spec/ruby3mf-testfiles/passing_cases/test_case_5.zip" }
-    let(:zip_file) { Zip::File.open(zip_path) }
-    let(:inputs)   { zip_file.each(&:get_input_stream) }
-    let(:types)    { ContentTypes.parse(inputs["[Content_Types].xml"]) }
+  before do
+    allow(zip_entry).to receive(:get_input_stream).and_return(content_xml)
+  end
+
+  describe ".parse a good file" do
+    let(:content_xml) {
+      '<?xml version="1.0" encoding="UTF-8"?>
+      <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+        <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml" />
+        <Default Extension="model" ContentType="application/vnd.ms-package.3dmanufacturing-3dmodel+xml" />
+      </Types>'
+    }
 
     it "should have valid content types" do
+      types = ContentTypes.parse(zip_entry)
       required_content_types.all? { |e| expect(types.values).to include(e) }
       expect(Log3mf.count_entries(:error, :fatal_error)).to be == 0
-      expect(Log3mf.count_entries(:fatal_error)).to be <= 1
     end
+  end
 
+  describe "invalid content type" do
+    let(:content_xml) {
+      '<?xml version="1.0" encoding="UTF-8"?>
+      <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+        <Default Extension="rels" ContentType="invalid content type" />
+        <Default Extension="model" ContentType="application/vnd.ms-package.3dmanufacturing-3dmodel+xml" />
+      </Types>'
+    }
+    let(:message) { 'missing required ContentType' }
+    it 'should report content type error' do
+      ContentTypes.parse(zip_entry)
+      expect(Log3mf.count_entries(:error, :fatal_error)).to be == 1
+      expect(Log3mf.count_entries(:fatal_error)).to be <= 1
+      expect(Log3mf.entries(:error).first[2]).to include message
+    end
   end
 
 end
