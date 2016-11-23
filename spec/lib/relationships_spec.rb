6d9bc5b35e1979e717dc5a28adaa72f9dfc0c138
Marlon Lopes <marlon.lopes@hp.com>
2016-11-23 11:21:36 -0200

    relationships specs

--- spec/lib/relationships_spec.rb
+++ spec/lib/relationships_spec.rb
@@ -2,37 +2,79 @@ require 'spec_helper'
 
 describe Relationships do
 
-  let(:model_zip_entry) { double('ZipEntry') }
+  context "when all relations are satisfied" do
 
-  let(:model_xml) {
-    '<?xml version="1.0" encoding="UTF-8"?>
-    <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-    <Relationship Target="/3D/3dmodel.model" Id="rel0" Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dmodel" />
-    </Relationships>'
-  }
+    let(:model_zip_entry) { double('ZipEntry', :name => "Relationships") }
 
-  let(:texture_zip_entry) { double('ZipEntry') }
+    let(:model_xml) {
+      '<?xml version="1.0" encoding="UTF-8"?>
+      <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+      <Relationship Target="/3D/3dmodel.model" Id="rel0" Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dmodel" />
+      </Relationships>'
+    }
 
-  let(:texture_xml) {
-    '<?xml version="1.0" encoding="utf-8"?>
-    <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-      <Relationship Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dtexture" Target="/3D/Texture/texture1.texture" Id="rel1" />
-      <Relationship Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dtexture" Target="/3D/Texture/texture2.texture" Id="rel2" />
-    </Relationships>'
-  }
+    let(:texture_zip_entry) { double('ZipEntry', :name => "Relationships") }
 
-  let(:rels) { [model_zip_entry, texture_zip_entry] }
+    let(:texture_xml) {
+      '<?xml version="1.0" encoding="utf-8"?>
+      <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+        <Relationship Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dtexture" Target="/3D/Texture/texture1.texture" Id="rel1" />
+        <Relationship Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dtexture" Target="/3D/Texture/texture2.texture" Id="rel2" />
+      </Relationships>'
+    }
 
-  before do
-    allow(model_zip_entry).to receive(:name).and_return("_rels/.rels")
+    let(:rels) { [model_zip_entry, texture_zip_entry] }
 
-    allow(model_zip_entry).to receive(:get_input_stream).and_return(model_xml)
-    allow(texture_zip_entry).to receive(:get_input_stream).and_return(texture_xml)
+    before do
+      allow(model_zip_entry).to receive(:get_input_stream).and_return(model_xml)
+      allow(texture_zip_entry).to receive(:get_input_stream).and_return(texture_xml)
+    end
+
+    it "should parse relationshios prolerly" do
+      inputs = rels.map{|r| Relationships.parse(r)}
+      expect(Log3mf.count_entries(:error, :fatal_error)).to be == 0
+    end
+
   end
 
+  context "when all relations are not satisfied" do
 
-  it "should parse relationshios prolerly" do
-    inputs = rels.map{|r| Relationships.parse(r)}
+    let(:model_zip_entry) { double('ZipEntry', :name => "Relationships") }
+
+    let(:model_xml) {
+      '<?xml version="1.0" encoding="UTF-8"?>
+      <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+      <Relationship Target="/3D/3dmodel.model" Id="rel0" Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dmodel" />
+      <FooNode Target="Bar" Id="rel0" Type="Baz" />
+      </Relationships>'
+    }
+
+    let(:texture_zip_entry) { double('ZipEntry', :name => "Relationships") }
+
+    let(:texture_xml) {
+      '<?xml version="1.0" encoding="utf-8"?>
+      <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+        <Relationship Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dtexture" Target="/3D/Texture/texture1.texture" Id="rel1" />
+        <Relationship Type="http://schemas.microsoft.com/3dmanufacturing/2013/01/3dtexture" Target="/3D/Texture/texture2.texture" Id="rel2" />
+      </Relationships>'
+    }
+
+    let(:rels) { [model_zip_entry, texture_zip_entry] }
+
+    let(:message) { "found non-Relationship node: FooNode" }
+
+    before do
+      allow(model_zip_entry).to receive(:get_input_stream).and_return(model_xml)
+      allow(texture_zip_entry).to receive(:get_input_stream).and_return(texture_xml)
+    end
+
+    it "should parse relationshios prolerly" do
+      inputs = rels.map{|r| Relationships.parse(r)}
+      expect(Log3mf.count_entries(:error, :fatal_error)).to be == 0
+      expect(Log3mf.entries(:info)[1][2]).to include message
+    end
+
   end
+
 
 end
