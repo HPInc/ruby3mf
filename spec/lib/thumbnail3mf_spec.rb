f1b16486b38b32ec1272dc6e7063164374515bd5
Marlon Lopes <marlon.lopes@hp.com>
2016-11-24 10:39:32 -0200

    dont rely on Zip

--- spec/lib/thumbnail3mf_spec.rb
+++ spec/lib/thumbnail3mf_spec.rb
@@ -2,15 +2,32 @@ require 'spec_helper'
 
 describe Thumbnail3mf do
 
-  let(:file_path) { "spec/ruby3mf-testfiles/failing_cases/3d_payload_files.3mf" }
-  let(:zip_file)  { Zip::File.open(file_path) }
-  let(:images)    { zip_file.glob('**/*texture') }
-  let(:message)   { "thumbnail is of type: image/jpeg"}
+  context ".parse good ContentType" do
+    let(:img_type)  { double('ImgType', :type => 'image/jpeg')}
+    let(:zip_entry) { double('ZipEntry', :get_input_stream => img_type)}
+    let(:images)    { double('Images', :first => zip_entry) }
+    let(:message)   { 'thumbnail is of type'}
 
-  it "The images may be of content type image/jpeg or image/png" do
-    ENV['LOGDEBUG']='true'
-    Thumbnail3mf.parse(:doc, images.first, :relationships)
-    expect(Log3mf.entries(:debug).first[2]).to include message
+    it "Should not log error and only log debug for content type image/jpeg or image/png" do
+      ENV['LOGDEBUG']='true'
+      allow(MimeMagic).to receive(:by_magic).and_return(img_type)
+      Thumbnail3mf.parse(:doc, images.first, :relationships)
+      expect(Log3mf.entries(:debug).first[2]).to include message
+    end
   end
+
+  context ".parse good ContentType" do
+    let(:img_type)  { double('ImgType', :type => 'foo/bar')}
+    let(:zip_entry) { double('ZipEntry', :get_input_stream => img_type)}
+    let(:images)    { double('Images', :first => zip_entry) }
+    let(:message)   { 'Expected a png or jpeg thumbnail but the thumbnail was of type'}
+
+    it "Should log error when no png or jpeg" do
+      allow(MimeMagic).to receive(:by_magic).and_return(img_type)
+      Thumbnail3mf.parse(:doc, images.first, :relationships)
+      expect(Log3mf.entries(:error).first[2]).to include message
+    end
+  end
+
 
 end
