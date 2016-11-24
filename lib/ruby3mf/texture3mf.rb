6707f73b6a87ed096a430052a74df669952f726c
Marlon Lopes <marlon.lopes@hp.com>
2016-11-24 12:06:21 -0200

    add texture3mf specs

--- lib/ruby3mf/texture3mf.rb
+++ lib/ruby3mf/texture3mf.rb
@@ -6,12 +6,12 @@ class Texture3mf
   end
 
   def self.parse(document, relationship_file, relationships)
-    
+
     t = new(document)
     t.name = relationship_file.name
-
-    img_type = MimeMagic.by_magic(relationship_file.get_input_stream)
-    @bytes = relationship_file.get_input_stream.read
+    stream = relationship_file.get_input_stream
+    img_type = MimeMagic.by_magic(stream)
+    @bytes = stream.read
     Log3mf.context "Texture3mf" do |l|
       l.debug "texture is of type: #{img_type}"
       l.error "Expected a png or jpeg texture but the texture was of type #{img_type}", spec: :material, page: 16 unless ['image/png', 'image/jpeg'].include? img_type.type
--- spec/lib/texture3mf_spec.rb
+++ spec/lib/texture3mf_spec.rb
@@ -2,5 +2,55 @@ require 'spec_helper'
 
 describe Texture3mf do
 
+  let(:bytes)             { "Dale Internacional !!!".bytes }
+  let(:document)          { double('Document', :objects => {}, :contents_for => bytes) }
+  let(:relationship_file) { double('ZipEntry', :name => 'name', :get_input_stream => double('Stream', :read => bytes))}
+
+  before do
+    allow(MimeMagic).to receive(:by_magic).and_return(img_type)
+    allow(MimeMagic).to receive(:name=)
+  end
+
+  context ".parse good ContentType" do
+    let(:img_type)          { double('ImgType', :type => 'image/jpeg')}
+    let(:message)           { 'texture is of type'}
+
+    it "Should not log error and only log debug for content type image/jpeg or image/png" do
+      ENV['LOGDEBUG']='true'
+      Texture3mf.parse(:doc, relationship_file, :relationships)
+      expect(Log3mf.entries(:debug).first[2]).to include message
+    end
+  end
+
+  context ".parse good ContentType" do
+    let(:img_type)  { double('ImgType', :type => 'foo/bar')}
+    let(:message)   { 'Expected a png or jpeg texture but the texture was of type'}
+
+    it "Should log error when no png or jpeg" do
+      Texture3mf.parse(:doc, relationship_file, :relationships)
+      expect(Log3mf.entries(:error).first[2]).to include message
+    end
+  end
+
+  context "#update @doc content" do
+    let(:img_type)  { double('ImgType', :type => 'image/jpeg')}
+    let(:texture)   { Texture3mf.parse(document, relationship_file, :relationships) }
+    let(:new_data)  { 'Dale Timbers !!!'.bytes}
+
+    it "Should log error when no png or jpeg" do
+      expect(texture.contents).not_to eq(new_data)
+      texture.update(new_data)
+      expect(texture.contents).to eq(new_data)
+    end
+  end
+
+  context ".contents should return texture bytes content" do
+    let(:img_type)  { double('ImgType', :type => 'image/jpeg')}
+    let(:texture)   { Texture3mf.parse(document, relationship_file, :relationships) }
+
+    it "#contents should return content for relationship_file" do
+      expect(texture.contents).to eq(bytes)
+    end
+  end
 
 end
