01eadb080610c9f576552f06117343ea95e472c7
Mike Whitmarsh <mike.whitmarsh@hp.com>
2017-01-04 15:28:51 -0800

    Global validations check for UTF-8 encoding SW3D-178

--- lib/ruby3mf/global_xml_validations.rb
+++ lib/ruby3mf/global_xml_validations.rb
@@ -1,5 +1,3 @@
-require 'benchmark'
-
 class GlobalXMLValidations
 
   def self.validate(document)
@@ -7,12 +5,23 @@ class GlobalXMLValidations
       if space_attribute_exists?(document)
         l.error "found an xml:space attribute when it is not allowed", page: 16
       end
+
+      if xml_not_utf8_encoded?(document)
+        l.error "found XML content that was not UTF8 encoded", page: 15
+      end
     end
   end
 
   def self.space_attribute_exists?(document)
     # using double negation as it's O(1) rather than the O(n) of .any?
     !document.xpath('//*[@xml:space]').empty?
+  end
+
+  def self.xml_not_utf8_encoded?(document)
+    if document.encoding == 'UTF-8'
+      return false
+    end
+    true
   end
 
 end
