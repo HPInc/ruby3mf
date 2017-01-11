c1e171e472adf2108827800936fb499efb5d2983
Mike Whitmarsh <mike.whitmarsh@hp.com>
2017-01-11 15:50:08 -0800

    updated Thumbnail to use new symbol-based errors SW3D-119

--- spec/lib/thumbnail3mf_spec.rb
+++ spec/lib/thumbnail3mf_spec.rb
@@ -8,20 +8,6 @@ describe Thumbnail3mf do
     allow(XmlVal).to receive(:validate).and_return(false)
   end
 
-  context ".parse good ContentType" do
-    let(:img_type)  { double('ImgType', :type => 'image/jpeg')}
-    let(:zip_entry) { double('ZipEntry', :get_input_stream => img_type)}
-    let(:images)    { double('Images', :first => zip_entry) }
-    let(:message)   { 'thumbnail is of type'}
-    let(:img_colorspace) { double('String', :colorspace => 'DirectClass sRGB Matte')}
-
-    it "Should not log error and only log debug for content type image/jpeg or image/png" do
-      ENV['LOGDEBUG']='true'
-      Thumbnail3mf.parse(:doc, images.first)
-      expect(Log3mf.entries(:debug).first[2]).to include message
-    end
-  end
-
   context ".parse bad ContentType" do
     let(:img_type)  { double('ImgType', :type => 'foo/bar')}
     let(:zip_entry) { double('ZipEntry', :get_input_stream => img_type)}
