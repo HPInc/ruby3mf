e2301e38d52f5de07b4341bd0275b9e1609e1971
Mike Whitmarsh <mike.whitmarsh@hp.com>
2017-01-12 17:10:01 -0800

    new ruby based suite test script. still working out the reporting...

---
+++ bin/suite.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/ruby3mf'
+
+# usage
+# bin/suite.rb {path to 3mf-test-suite} optional
+# assumes . is ~/src/ruby3mf and that ~/src/3mf-test-suite is path to suite repo files
+
+$stdout.sync = true
+
+good_files = Dir["#{ARGV[0] || '../3mf-test-suite'}/Positive/*.3mf"]
+bad_files = Dir["#{ARGV[0] || '../3mf-test-suite'}/Negative/*.3mf"]
+
+false_negatives = {}
+true_negatives={}
+false_positives = []
+
+puts "\n\nPositive"
+good_files.each do |file|
+  print "." # "Validating file: #{file}"
+  Log3mf.reset_log
+  doc = Document.read(file)
+  errors = Log3mf.entries(:fatal_error, :error)
+
+  if errors.size > 0
+    false_negatives[file]=errors
+    puts "\n#{file}"
+    errors.each do |ent|
+      h = {context: ent[0], severity: ent[1], message: ent[2]}
+      puts "  #{h}"
+    end
+  end
+
+end
+
+puts "\n\nNegative"
+bad_files.each do |file|
+  print "." #puts "Validating file #{file}"
+  Log3mf.reset_log
+  doc = Document.read(file)
+  errors = Log3mf.entries(:fatal_error, :error)
+  if errors.size > 0
+    true_negatives[file] = errors
+  else
+    false_positives << file
+    puts "\n#{file} - No Errors Found!"
+  end
+end
