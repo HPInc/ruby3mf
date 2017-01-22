393cf6558f8bbabf34d3bed0bdbc70fd1a355383
Marlon Lopes <marlon.lopes@hp.com>
2017-01-22 09:08:08 -0200

    xsd content memoization

--- lib/ruby3mf/schema_files.rb
+++ lib/ruby3mf/schema_files.rb
@@ -5,8 +5,14 @@ class SchemaFiles
   SchemaTemplate  = File.join(File.dirname(__FILE__), "3MFcoreSpec_1.1.xsd.template")
   SchemaLocation  = File.join(File.dirname(__FILE__), "xml.xsd")
 
-  def self.render(template)
-    ERB.new(template).result( binding )
+  class << self
+    attr_accessor :xsd_content
+
+    def render(template)
+       @@xsd_content ||= ERB.new(template).result( binding )
+    end
+
   end
+
 
 end
