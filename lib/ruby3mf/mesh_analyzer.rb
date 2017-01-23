a53b46feae3797910a77b24b289b328eb2bcc4a2
Marlon Lopes <marlon.lopes@hp.com>
2017-01-23 15:13:33 -0200

    WIP validating NC_411

--- lib/ruby3mf/mesh_analyzer.rb
+++ lib/ruby3mf/mesh_analyzer.rb
@@ -24,15 +24,17 @@ class MeshAnalyzer
       # if a triangle has a pid, then the object needs a pid
       has_triangle_pid = false
 
-      mesh = find_child(object, "mesh")
-      if mesh
-        triangles = find_child(mesh, "triangles")
+      meshs = object.css('mesh')
+      meshs.each do |mesh|
+
+        triangles = mesh.css("triangle")
 
         if triangles
-          triangles.children.each do |triangle|
-            v1 = triangle.attributes["v1"].to_s().to_i()
-            v2 = triangle.attributes["v2"].to_s().to_i()
-            v3 = triangle.attributes["v3"].to_s().to_i()
+          triangles.each do |triangle|
+
+            v1 = triangle.attributes["v1"].to_s.to_i
+            v2 = triangle.attributes["v2"].to_s.to_i
+            v3 = triangle.attributes["v3"].to_s.to_i
 
             if v1 == v2 || v2 == v3 || v3 == v1
               l.error :non_distinct_indices
@@ -41,18 +43,16 @@ class MeshAnalyzer
             list.add_edge(v1, v2)
             list.add_edge(v2, v3)
             list.add_edge(v3, v1)
-
-            if not has_triangle_pid
+            if !has_triangle_pid
               has_triangle_pid = triangle.attributes["pid"] != nil
             end
           end
 
-          has_object_material = object.attributes["pid"] and object.attributes["pindex"]
-          if has_triangle_pid and not has_object_material
+          if has_triangle_pid && !(object.attributes["pindex"] && object.attributes["pid"])
             l.error :missing_object_pid
           end
 
-          result = list.verify_edges()
+          result = list.verify_edges
           if result == :bad_orientation
             l.fatal_error :resource_3dmodel_orientation
           elsif result == :hole
@@ -60,26 +60,16 @@ class MeshAnalyzer
           elsif result == :nonmanifold
             l.fatal_error :resource_3dmodel_nonmanifold
           end
+
         end
       end
     end
   end
 
   def self.validate(model_doc)
-    root = model_doc.root
-    node = root
-
-    if node.name == "model"
-      resources = find_child(node, "resources")
-
-      if resources
-        resources.children.each do |resource|
-          solid_model = resource.attributes["type"].to_s() == "model" or resource.attributes["type"].to_s() == "solidsupport"
-          if resource.name == "object" and solid_model
-            validate_object(resource)
-          end
-        end
-      end
+    model_doc.css('model/resources/object').select{ |object| ['model', 'solidsupport', 'support', 'other', ''].include?(object.attributes['type'].to_s) }.each do |object|
+      validate_object(object)
     end
   end
+
 end
