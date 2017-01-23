d61b3956cf399fc1d5e4c18ee18a873758f58d23
Marlon Lopes <marlon.lopes@hp.com>
2017-01-23 12:15:40 -0200

    Revert "Revert "Revert "WIP properly check errors for when error in vertices"""
    This reverts commit 46267ea1f9d8b22f4dcae88a73469cd3af0ce1bd.

--- lib/ruby3mf/mesh_analyzer.rb
+++ lib/ruby3mf/mesh_analyzer.rb
@@ -12,7 +12,6 @@ end
 class MeshAnalyzer
 
   def self.validate_object(object)
-
     Log3mf.context "verifying object" do |l|
       children = object.children.map { |child| child.name }
       have_override = object.attributes["pid"] or object.attributes["pindex"]
@@ -25,72 +24,62 @@ class MeshAnalyzer
       # if a triangle has a pid, then the object needs a pid
       has_triangle_pid = false
 
-      # mesh = find_child(object, "mesh")
-      meshs = object.css('mesh')
+      mesh = find_child(object, "mesh")
+      if mesh
+        triangles = find_child(mesh, "triangles")
 
-      meshs.each do |mesh|
+        if triangles
+          triangles.children.each do |triangle|
+            v1 = triangle.attributes["v1"].to_s().to_i()
+            v2 = triangle.attributes["v2"].to_s().to_i()
+            v3 = triangle.attributes["v3"].to_s().to_i()
 
-        # l.error :non_distinct_indices if mesh.css('triangle').map{ |triangle| [ triangle.attributes["v1"].value, triangle.attributes["v2"].value, triangle.attributes["v3"].value ] }.map(&:uniq).map(&:size).any?{ |v| v != 3 }
+            if v1 == v2 || v2 == v3 || v3 == v1
+              l.error :non_distinct_indices
+            end
 
-        # triangles = find_child(mesh, "triangles")
-        triangles = mesh.css('triangle')
+            list.add_edge(v1, v2)
+            list.add_edge(v2, v3)
+            list.add_edge(v3, v1)
 
-        triangles.each do |triangle|
-          v1 = triangle.attributes["v1"].to_s.to_i
-          v2 = triangle.attributes["v2"].to_s.to_i
-          v3 = triangle.attributes["v3"].to_s.to_i
-
-          if v1 == v2 || v2 == v3 || v3 == v1
-            l.error :non_distinct_indices
+            if not has_triangle_pid
+              has_triangle_pid = triangle.attributes["pid"] != nil
+            end
           end
 
-          list.add_edge(v1, v2)
-          list.add_edge(v2, v3)
-          list.add_edge(v3, v1)
+          has_object_material = object.attributes["pid"] and object.attributes["pindex"]
+          if has_triangle_pid and not has_object_material
+            l.error :missing_object_pid
+          end
 
-          if not has_triangle_pid
-            has_triangle_pid = triangle.attributes["pid"] != nil
+          result = list.verify_edges()
+          if result == :bad_orientation
+            l.fatal_error :resource_3dmodel_orientation
+          elsif result == :hole
+            l.fatal_error :resource_3dmodel_hole
+          elsif result == :nonmanifold
+            l.fatal_error :resource_3dmodel_nonmanifold
           end
         end
-
-        has_object_material = object.attributes["pid"] && object.attributes["pindex"]
-        if has_triangle_pid and not has_object_material
-          l.error :missing_object_pid
-        end
-
-        result = list.verify_edges()
-        if result == :bad_orientation
-          l.error :resource_3dmodel_orientation
-        elsif result == :hole
-          l.error :resource_3dmodel_hole
-        elsif result == :nonmanifold
-          l.error :resource_3dmodel_nonmanifold
-        end
-
       end
-
     end
   end
 
   def self.validate(model_doc)
-    # root = model_doc.root
-    # node = root
+    root = model_doc.root
+    node = root
 
-    model_doc.css('object').each do |object|
-      validate_object(object) if ["model", "solidsupport", nil, ""].any?{ |type| type == object.attributes['type'].to_s }
+    if node.name == "model"
+      resources = find_child(node, "resources")
+
+      if resources
+        resources.children.each do |resource|
+          solid_model = resource.attributes["type"].to_s() == "model" or resource.attributes["type"].to_s() == "solidsupport"
+          if resource.name == "object" and solid_model
+            validate_object(resource)
+          end
+        end
+      end
     end
-
-    # if node.name == "model"
-    #   resources = find_child(node, "resources")
-    #
-    #   if resources
-    #     resources.children.each do |resource|
-    #       if resource.name == "object" || ( resource.attributes["type"].to_s == "model" || resource.attributes["type"].to_s == "solidsupport" )
-    #         validate_object(resource)
-    #       end
-    #     end
-    #   end
-    # end
-
   end
 end
